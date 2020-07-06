bd5b7ed96eea0f9fc2c48d30866989106de1fb0c
Gabriel Ribeiro <49738156+Gab1235@users.noreply.github.com>
2020-07-05 22:06:32 -0400

    added fixes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,7 +19,6 @@ export default function Home() {
     //   password,
     // });
 
-    // api.defaults.headers["Authorization"] = `Bearer ${response.headers["set-cookie"][0]}`;
     // await AsyncStorage.setItem("token", response.headers["set-cookie"][0]);
 
     navigation.navigate("Deliveries");
